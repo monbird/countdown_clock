6e2f7a6e82bca587b33bb7b89a88971f31838a3c
Monika Ptaszynska <mon.ptasz@gmail.com>
2019-08-01 02:02:09 +0100

    update READEME file, update HTML tree, improve CSS-layout & responsiveness, modify js-add input validation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# countdown_clock
+# Countdown Clock
+
+This project focuses on JavaScript date & time objects. The created clock widget counts down the remaining time unitl the date provided by the user.
+
+The page is fully responsive and according input validation is performed.
+
+To view completed project click [here](http://monbird.com/countdown-clock).
--- logic.js
+++ logic.js
@@ -1,71 +1,85 @@
 $(document).ready(function () {
 
-    var clockInterval;
+    var clockInterval;      // initialize global variable
 
+    // define on click behaviour for #countButton
     $('#countButton').click(function () {
 
-        if (!isEmpty()) {
-            clearInterval(clockInterval);
+        $("#message").html("&nbsp;").removeClass("fail success");       //reset to default settings
+
+        if (isValid()) {        // if condition is met counting begins
+            clearInterval(clockInterval);       // reset interval
             startCountdown();
         }
     });
 
-    function isEmpty() {
-        if($('#datePicker').val() == "" && $('#timePicker').val() == "") {
-            alert("Date and time have not been selected. Please choose your date and time.");
-            return true;
-        } else if ($('#datePicker').val() == "") {
-            alert("No date has been selected. Please choose your date.");
-            return true;
-        } else if ($('#timePicker').val() == "") {
-            alert("No time has been selected. Please choose your time.");
-            return true;
-        } else {
+    function readDate() {
+
+        // check if user provided time if not set to 00:00
+        if ($('#timePicker').val() == "") {
+            $('#timePicker').val("00:00")
+        }
+
+        var countDownDate = new Date($('#datePicker').val() + " " + $('#timePicker').val()).getTime();
+        return countDownDate;
+    }
+
+    // input validation
+    function isValid() {
+        if ($('#datePicker').val() == "") {     // checks if empty
+            $("#message").text("Please choose your date.").addClass("fail");
             return false;
+        } else if (readDate() <= new Date().getTime()) {        // checks if past date
+            $("#message").text("You have choosen date from the past. Please choose a new date.").addClass("fail");
+            return false;
+        } else {
+            return true;
         }
     }
 
+    // counting down function
     function startCountdown() {
 
-        // Set the date we're counting down to
-        var countDownDate = new Date($('#datePicker').val() + " " + $('#timePicker').val()).getTime();
+        // set the date we're counting down to
+        var countDownDate = readDate();
 
-        // Update the count down every 1 second
-        clockInterval = setInterval(function() {    // par1 funkcja ktora wywoluje sie co sekunde (par2 - 1000)
+        // update the count down every 1 second
+        clockInterval = setInterval(function() {    // argument1 -  function which is exectued every second (argument2 - 1000 miliseconds = 1s)
 
-            // Get today's date and time
+            // get today's date and time
             var now = new Date().getTime();
         
-            // Find the difference between now and the count down date
+            // find the difference between now and the countDownDate
             var difference = countDownDate - now;
 
+            // condition to escape the function
             if (difference < 0) {
                 $("#days").text("0");
                 $("#hours").text("0");
                 $("#minutes").text("0");
                 $("#seconds").text("0");
+
+                $("#message").text("Countdown finished!").removeClass("fail").addClass("success");
                 
-                clearInterval(clockInterval);   // poprez wyczysczenie nie wywola sie funkcja ponownie
+                clearInterval(clockInterval);   // clear the interval so that the function stops
                 return;
-                }
+            }
         
-            // Time calculations for days, hours, minutes and seconds
+            // time calculations for days, hours, minutes and seconds
             var days = Math.floor(difference / (1000 * 60 * 60 * 24));
             var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
             var seconds = Math.floor((difference % (1000 * 60)) / 1000);
         
-            // Display the results within the corresponding elements
+            // isplay the results within the corresponding elements
             $("#days").text(days);
             $("#hours").text(hours);
             $("#minutes").text(minutes);
             $("#seconds").text(seconds);
 
-            // If the count down is finished, write some text 
-            
-
-            }, 1000);
+        }, 1000);
     }
 });
+
 
 
